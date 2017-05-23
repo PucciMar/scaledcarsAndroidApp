f70bfb1146e11c0a27a1dbb8ef0088a8e6163af8
PuccinelliRaphael <raphaelpuccinelli@gmail.com>
2017-05-23 22:08:33 +0200

    deleting duplicate

--- scaledCarsOpendanvinciExamples/udpconnectionreceiver/apps/scaledcars-control-udpconnectionreceiver.cpp
+++
@@ -1,6 +0,0 @@
-#include "UDPConnectionReceiver.h"
-
-int32_t main(int32_t argc, char **argv) {
-    scaledcars::control::UDPConnectionReceiver udpconnectionreceiver(argc, argv);
-    return udpconnectionreceiver.runModule();
-}
--- scaledCarsOpendanvinciExamples/udpconnectionstreamer/apps/scaledcars-control-udpconnectionstreamer.cpp
+++
@@ -1,6 +0,0 @@
-#include "UDPConnectionStreamer.h"
-
-int32_t main(int32_t argc, char **argv) {
-    scaledcars::control::UDPConnectionStreamer udpconnectionstreamer(argc, argv);
-    return udpconnectionstreamer.runModule();
-}
